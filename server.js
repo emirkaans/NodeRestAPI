a91119c94d22323c9d3b88145eda54c62b150796
Emir <emirkaansaricam@gmail.com>
2024-01-19 01:07:02 +0300

    dev done

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/productModel");
 const app = express();
+
+app.use(express.json());
 
 // routes
 
@@ -7,6 +11,86 @@ app.get("/", (req, res) => {
   res.send("Hello Node API");
 });
 
-app.listen(3000, () => {
-  console.log("Node API app is running on port 3000");
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `The Product ${id} cannot be found` });
+    }
+
+    const updatedProduct = await Product.findById(id);
+
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `The Product ${id} cannot be found` });
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://emirkaansaricam:Vairo123*@nodeapi.tnitueq.mongodb.net/NodeAPI"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    app.listen(3000, () => {
+      console.log("Node API app is running on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
